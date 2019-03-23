6e546d2499d7e1b60ca35bfc7f111928aac532ff
Charles Swaleh <charlswall@live.com>
2019-03-23 12:21:58 +0300

    debugging in javascript

---
+++ js/debugging.js
@@ -0,0 +1,17 @@
+      $(document).ready(function() {
+          $("#blanks form").submit(function() {
+
+              var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+              blanks.forEach(function(blank) {
+
+                  var userInput = $("input#" + blank).val();
+
+                  $("." + blank).text(userInput);
+              });
+
+              $("#story").show();
+              event.preventDefault();
+
+          });
+      });
